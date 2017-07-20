7963f54fc08431ef1d8583f27d89e4ccb30dae1a
zhaolii <zhaolili@100tal.com>
2017-07-20 08:20:11 +0800

    detail

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,33 +29,38 @@
 		<div class="background">
 			<img :src="seller.avatar" width='100%' height="100%">
 		</div>
-		<div v-show="detailShow" class="detail">
-			<div class="detail-wraper clearfix">
-				<div class="detail-main">
-					<h1 class="name">{{seller.name}}</h1>
-					<div class="star-wrapper"><star :score="seller.score" :size="48"></star></div>
-					<div class="title">
-						<div class="line"></div>
-						<div class="text">优惠信息</div>
-						<div class="line"></div>
-					</div>
-					<!-- <ul v-if="seller.supports" class="supports">
-						<li class="support-item" v-for="(item,index) in seller.supports">
-							<span class="icon" :class="classMap[seller.supports[index].type]"></span>
-							<span class="text">{{seller.supports[index].description}}</span>
-						</li>
-					</ul> -->
-					<div class="title">
-						<div class="line"></div>
-						<div class="text">商家公告</div>
-						<div class="line"></div>
+		<transition name="fade">
+			<div v-show="detailShow" class="detail">
+				<div class="detail-wraper clearfix">
+					<div class="detail-main">
+						<h1 class="name">{{seller.name}}</h1>
+						<div class="star-wrapper"><star :score="seller.score" :size="48"></star></div>
+						<div class="title">
+							<div class="line"></div>
+							<div class="text">优惠信息</div>
+							<div class="line"></div>
+						</div>
+						<ul v-if="seller.supports" class="supports">
+							<li class="support-item" v-for="(item,index) in seller.supports">
+								<span class="icon" :class="classMap[seller.supports[index].type]"></span>
+								<span class="text">{{seller.supports[index].description}}</span>
+							</li>
+						</ul>
+						<div class="title">
+							<div class="line"></div>
+							<div class="text">商家公告</div>
+							<div class="line"></div>
+						</div>
+						<div class="bulletin">
+							<p class="content">{{seller.bulletin}}</p>
+						</div>
 					</div>
 				</div>
+				<div class="detail-close" @click="hideDetail">
+					<i class="icon-close"></i>
+				</div>
 			</div>
-			<div class="detail-close">
-				<i class="icon-close"></i>
-			</div>
-		</div>
+		</transition>
 	</div>
 </template>
 <script>
@@ -75,6 +80,9 @@
 		methods: {
 			showDetail() {
 				this.detailShow = true;
+			},
+			hideDetail() {
+				this.detailShow = false;
 			}
 		},
 		created() {
@@ -207,7 +215,13 @@
 			width:100%
 			height:100%
 			overflow:auto
-			background:rgba(7,17,27,0.8)
+			transition:all 0.5s
+			&.fade-transition
+				opacity:1
+				background:rgba(7,17,27,0.8)
+			&.fade-enter,&.fade-leave
+				opacity:0
+				background:rgba(7,17,27,0)
 			.detail-wraper
 				width:100%
 				min-height:100%
@@ -236,14 +250,43 @@
 							font-size:14px
 							font-weight:700
 							padding:0 12px
-					.supports{
+					.supports
 						width:80%
 						margin:0 auto
-						// .support-item
-						// 	padding:0 12px
-						// 	margin-bottom:12px
-						// 	font-size:0
-					}
+						.support-item
+							padding:0 12px
+							margin-bottom:12px
+							font-size:0
+							&:last-child
+								margin-bottom:0
+							.icon
+								display:inline-block
+								vertical-align:top
+								width:16px
+								height:16px
+								margin-right:6px
+								background-size:16px 16px
+								background-repeat:no-repeat
+								&.decrease
+									bg-image('decrease_2')
+								&.discount
+									bg-image('discount_2')
+								&.guarantee
+									bg-image('guarantee_2')
+								&.invoice
+									bg-image('invoice_2')
+								&.special
+									bg-image('special_2')
+							.text
+								font-size:12px
+								line-height:16px
+					.bulletin
+						width:80%
+						margin:0 auto
+						.content
+							padding:0 12px
+							line-height:24px
+							font-size:12px
 			.detail-close
 					position:relative
 					width:32px
