f3afe64c76c242af4448c7ff5fd5ae00f7eaa9c3
zhaolii <zhaolili@100tal.com>
2017-07-24 08:04:20 +0800

    goods

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,20 +1,45 @@
 <template>
 	<div class="goods">
-		<div class="menu-wrapper">
+		<div class="menu-wrapper" v-el:menu-wrapper>
 			<ul>
-				<li v-for="item in goods">
-					<span class="text">
-						<span v-show="item.type>0" class="icon" :class=""></span>{{item.name}}
+				<li v-for="item in goods" class="menu-item">
+					<span class="text border-1px bottom">
+						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
 					</span>
 				</li>
 			</ul>
 		</div>
-		<div class="foods-wrapper">
-
+		<div class="foods-wrapper" v-el:foodsWrapper>
+			<ul>
+				<li v-for="item in goods" class="food-list">
+					<h1 class="title">{{item.name}}</h1>
+					<ul>
+						<li v-for="food in item.foods" class="food-item border-1px bottom">
+							<div class="icon">
+								<img width="57px" height="57px" :src="food.icon" />
+							</div>
+							<div class="content">
+								<h2 class="name">{{food.name}}</h2>
+								<p class="desc">{{food.description}}</p>
+								<div class="extra">
+									<span class="count">月售{{food.sellCount}}份</span>
+									<span>好评率{{food.rating}}%</span>
+								</div>
+								<div class="price">
+									<span class="new">¥{{food.price}}</span>
+									<span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
 <script>
+	import BScroll from 'better-scroll';
+	
 	const ERR_OK = 0;
 	export default {
 		props: {
@@ -28,17 +53,28 @@
 			};
 		},
 		created() {
+			this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
 			this.$http.get('/api/goods').then((response) => {
 				response = response.body;
 				if (response.errno === ERR_OK) {
 					this.goods = response.data;
-					console.log(this.goods);
+					this.$nextTick(() => {
+						this._initScroll();
+					});
 				}
 			});
+		},
+		methods: {
+			_initScroll() {
+				this.menuScroll = new BSscroll(this.$els.menuWrapper, {});
+				this.foodsScroll = new BSscroll(this.$els.foodsWrapper, {});
+			}
 		}
 	};
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+	@import "../../common/stylus/mixin.styl"
+
 	.goods
 		display:flex
 		position:absolute
@@ -50,7 +86,83 @@
 			flex:0 0 80px
 			width:80px
 			background:#f3f5f7
+			.menu-item
+				display:table
+				width:56px
+				height:54px
+				margin:0 12px
+				line-height:14px
+				.icon
+					display:inline-block
+					vertical-align:top
+					width:12px
+					height:12px
+					margin-right:2px
+					background-size:12px 12px
+					background-repeat:no-repeat
+					&.decrease
+						bg-image('decrease_3')
+					&.discount
+						bg-image('discount_3')
+					&.guarantee
+						bg-image('guarantee_3')
+					&.invoice
+						bg-image('invoice_3')
+					&.special
+						bg-image('special_3')
+				.text
+					display:table-cell
+					width:56px
+					vertical-align:middle
+					border-1px(rgba(7,17,27,0.1))
+					font-size:12px
 		.foods-wrapper
 			flex:1
+			.title
+				padding-left:14px
+				height:26px
+				line-height:26px
+				border-left:2px solid #d9dde1
+				font-size:12px
+				color:rgb(147,153,159)
+				background:#f3f5f7
+			.food-item
+				display:flex
+				margin:18px 18px 0
+				padding-bottom:18px
+				border-1px(rgba(7,17,27,0.1))
+				&:last-child
+					border-none()
+				.icon
+					flex:0 0 57px
+					margin-right:10px
+				.name
+					margin:2px 0 8px 0
+					height:14px
+					line-height:14px
+					font-size:14px
+					color:rgb(7,17,27)
+				.desc,.extra
+					line-height:10px 
+					font-size:10px
+					color:rgb(147,153,159)
+				.desc 
+					margin-bottom:8px
+				.extra 
+					&.count
+						margin-right:13px
+				.price
+					font-weight:700
+					line-height:24px
+					.new
+						margin-right:8px
+						font-size:14px
+						color:rgb(240,20,20)
+					.old 
+						text-decoration:line-through
+						font-size:10 
+						color:rgb(147,153,159)
+						
+					
 
 </style>
